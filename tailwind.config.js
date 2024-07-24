a03a817594c9aa32078787d6fc0281114a372207
dmcjjj <73011311+dmcjjj@users.noreply.github.com>
2024-07-24 22:25:55 +1000

    update tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        // You can extend Tailwind's default theme here
+      },
+    },
+    plugins: [],
+  }
